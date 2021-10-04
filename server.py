8263938a2419e0cefd48cc495b7e22837dd3aaf5
chandan purohit <vyasapurohit@gmail.com>
2021-10-04 19:01:47 +0530

    Add enquiry done!

--- server.py
+++ server.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import database as db
 app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/addEnquiry",methods=['GET', 'POST'])
+def addEnquiry():
+    if request.method == "GET":
+        status = request.args.get("status",0)
+        return render_template("enquiry.html", enquiries=db.getEnquiries(),status=status)
+    else:
+        student_name = request.form['student']
+        phone = request.form['phone']
+        course_name = request.form['course']
+        status = db.addEnquiry(student_name, phone, course_name)
+        return redirect("/addEnquiry?status="+status)
 
 app.run(debug=True), 
